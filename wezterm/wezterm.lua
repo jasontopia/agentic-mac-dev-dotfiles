19a0f94a4f521f85a0d4431043fcc7535f864366
Jasontopia <admin@Admins-Mac-mini.local>
2026-09-15 13:23:01 -0600

    fix: remove invalid copy_to_clipboard_after_select field in wezterm

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,17 +1,17 @@
 local wezterm = require 'wezterm'
 local config = wezterm.config_builder()
 
--- Remove titlebar for borderless look
+-- Appearance & Theme (Rosé Pine Moon)
+config.color_scheme = 'rose-pine-moon'
 config.window_decorations = "RESIZE"
-
--- Hide tab bar when single tab
 config.hide_tab_bar_if_only_one_tab = true
-
--- Background transparency (88%) and macOS blur
 config.window_background_opacity = 0.88
 config.macos_window_background_blur = 20
 
--- Font size
+-- Font Settings
 config.font_size = 14.0
+
+-- Scrollback Lines
+config.scrollback_lines = 10000
 
 return config
